449a8a320eabb572a0c3b0cf310c89326b01ec0a
Cameron Smith <ca.smith657@gmail.com>
2017-06-12 12:20:50 -0600

    Placed assets in search results activity

--- app/src/main/java/com/example/tyler/warehousemanagement/SearchPage.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/SearchPage.java
@@ -1,6 +1,8 @@
 package com.example.tyler.warehousemanagement;
 
 import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class SearchPage extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+
+        //Get intent verify action and get query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+
+    private void doSearch(String query) {
+        //to be implemented later
     }
 }
