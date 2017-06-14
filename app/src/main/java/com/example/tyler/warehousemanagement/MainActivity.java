6374660859745c6c76226d365720663ba2aea255
Cameron Smith <ca.smith657@gmail.com>
2017-06-14 13:07:25 -0600

    connected search activities to main (need to be changed)

--- app/src/main/java/com/example/tyler/warehousemanagement/MainActivity.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.tyler.warehousemanagement;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,14 +11,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import org.junit.Test;
-import java.util.regex.Pattern;
 
 /**************************************
  *
@@ -106,6 +101,10 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.app_bar_search) {
+            //invoke the search_activate activity
+            Intent intent = new Intent(this, Search_Activate.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/tyler/warehousemanagement/Search_Activate.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/Search_Activate.java
@@ -2,6 +2,7 @@ package com.example.tyler.warehousemanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Search_Activate extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Search_Activate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search__activate);
+    }
+
+    public void doSearch(View view) {
+        onSearchRequested();
     }
 }
