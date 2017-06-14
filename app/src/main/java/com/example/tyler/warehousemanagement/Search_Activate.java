2d9c339a19d2b9e07c4519e7ff0263a07fb4bf95
Cameron Smith <ca.smith657@gmail.com>
2017-06-14 11:45:27 -0600

    created search_activate activity

---
+++ app/src/main/java/com/example/tyler/warehousemanagement/Search_Activate.java
@@ -0,0 +1,13 @@
+package com.example.tyler.warehousemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Search_Activate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search__activate);
+    }
+}
