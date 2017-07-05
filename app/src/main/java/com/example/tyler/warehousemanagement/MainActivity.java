f80bcb51f3ddb895d7c6242c845e21aad1052dfc
Cameron Smith <ca.smith657@gmail.com>
2017-07-05 13:10:18 -0600

    Merge branches 'master' and 'search' of https://github.com/Tyler2517/WareHouseManagement into search
    # Conflicts:
    #	app/src/main/java/com/example/tyler/warehousemanagement/MainActivity.java

--- app/src/main/java/com/example/tyler/warehousemanagement/MainActivity.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/MainActivity.java
@@ -2,10 +2,12 @@
         package com.example.tyler.warehousemanagement;
 
         import android.app.DialogFragment;
+        import android.app.SearchManager;
         import android.content.Context;
         import android.support.design.widget.TabLayout;
         import android.support.design.widget.FloatingActionButton;
         import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.SearchView;
         import android.support.v7.widget.Toolbar;
 
         import android.support.v4.app.Fragment;
@@ -98,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
                 newFragment.show(getFragmentManager(), "dialog");
             }
         });
-
 
     }
 
