41f135f73100c37da33aff5fa2fa8be72054c6bc
GanryuReydelosMoguris <bigpete@byui.edu>
2017-07-05 13:35:52 -0600

    Sorts Done

--- app/src/main/java/com/example/tyler/warehousemanagement/CondTab.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/CondTab.java
@@ -12,21 +12,35 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class CondTab extends Fragment {
     List<Item> Data;
-    public CondTab(List<Item> incData){
-        Data = incData;
+    List<Item> CondData;
+
+    public CondTab(List<Item> incCondData){
+        Data = incCondData;
+        CondData = new ArrayList<>(Data);
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.tab1, container, false);
 
-        String[] Practice = new String[Data.size()];
-        for(int i = 0; i < Data.size();i++) {
-            Practice[i] = Data.get(i).Name + " - " + Data.get(i).Name + " - " + Data.get(i).ID + " - " + Data.get(i).Location + " - " + Data.get(i).Condition;
+        // Sorting
+        Collections.sort(CondData, new Comparator<Item>() {
+            @Override
+            public int compare(Item item2, Item item1)
+            {
+
+                return  item1.Condition.compareTo(item2.Condition);
+            }
+        });
+        String[] Practice = new String[CondData.size()];
+        for(int i = 0; i < CondData.size();i++) {
+            Practice[i] = CondData.get(i).Name + " - " + CondData.get(i).ID + " - " + CondData.get(i).Location + " - " + CondData.get(i).Condition;
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, Practice);
         ListView list = (ListView) rootView.findViewById(R.id.ListViewTab1);
--- app/src/main/java/com/example/tyler/warehousemanagement/NameTab.java
+++ app/src/main/java/com/example/tyler/warehousemanagement/NameTab.java
@@ -12,21 +12,35 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class NameTab extends Fragment {
     List<Item> Data;
-    public NameTab(List<Item> incData){
-        Data = incData;
+    List<Item> NameData;
+
+    public NameTab(List<Item> incNameData){
+        Data = incNameData;
+        NameData = new ArrayList<>(Data);
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.tab1, container, false);
 
-        String[] Practice = new String[Data.size()];
-        for(int i = 0; i < Data.size();i++) {
-            Practice[i] = Data.get(i).Name + " - " + Data.get(i).Name + " - " + Data.get(i).ID + " - " + Data.get(i).Location + " - " + Data.get(i).Condition;
+        // Sorting
+        Collections.sort(NameData, new Comparator<Item>() {
+            @Override
+            public int compare(Item item2, Item item1)
+            {
+
+                return  item2.Name.compareTo(item1.Name);
+            }
+        });
+        String[] Practice = new String[NameData.size()];
+        for(int i = 0; i < NameData.size();i++) {
+            Practice[i] = NameData.get(i).Name + " - " + NameData.get(i).ID + " - " + NameData.get(i).Location + " - " + NameData.get(i).Condition;
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, Practice);
         ListView list = (ListView) rootView.findViewById(R.id.ListViewTab1);
